2269a32b91f5df8ab5b1578f87238a5bdfbce18e
masatora <atarashi.masatora@gmail.com>
2021-04-25 01:33:38 +0900

    Init get command

---
+++ cmd/get.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get progress",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("get called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+}
