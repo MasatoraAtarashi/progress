fe13fa03974197fee16938ba3b0ffe56c1612082
masatora <atarashi.masatora@gmail.com>
2021-04-25 03:54:42 +0900

    Implement function of get commit from multiple repository

--- cmd/get.go
+++ cmd/get.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/cobra"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -25,15 +26,32 @@ func runGetCmd(cmd *cobra.Command, args []string) (err error) {
 	if len(config.Repositories) <= 0 {
 		return errors.New("リポジトリを指定してください")
 	}
+
+	var output string
+	err = addMetaDate(&output)
+
 	for _, repository := range config.Repositories {
-		fmt.Println(repository)
+		repository_name := strings.Split(repository, "/")
+		output += "##" + repository_name[len(repository_name)-1] + "\n\n"
+
+		var commits string
+		commits, err = getProgress(repository, "masatora", "2021-04-25 00:00:00")
+		output += commits
+		output += "\n\n"
 	}
-	err = getProgress(config.Repositories[0], "masatora", "2021-04-25 00:00:00")
+
+	execLess(output)
 	return
 }
 
+// 出力にメタデータを追加
+func addMetaDate(output *string) (err error) {
+	*output += "#" + "2021-04-25\n\n"
+	return nil
+}
+
 // 指定した日に指定したユーザが行ったコミットを表示する
-func getProgress(repository string, username string, date string) (err error) {
+func getProgress(repository string, username string, date string) (output string, err error) {
 	layout := "2006-01-02 00:00:00"
 	start_date, err := time.Parse(layout, date)
 	end_date := start_date.AddDate(0, 0, 1)
@@ -45,8 +63,17 @@ func getProgress(repository string, username string, date string) (err error) {
 		"--since=" + start_date.Format(layout),
 		"--until=" + end_date.Format(layout),
 	)
-	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	out, err := cmd.Output()
+	output = string(out)
+	return
+}
+
+// lessコマンドで表示
+func execLess(str string) (err error) {
+	cmd := exec.Command("less", "-R")
+	cmd.Stdin = strings.NewReader(str)
+	cmd.Stdout = os.Stdout
 	err = cmd.Run()
 	return
 }
